17f97cf911169a36f4f3a1d4ed4a5d208d35e47c
Ana Cobian <ana.naiboc@gmail.com>
2019-01-25 10:29:16 -0800

    Update samsoft2fasta_sihits.pl
    added use

--- samsoft2fasta_sihits.pl
+++ samsoft2fasta_sihits.pl
@@ -1,6 +1,6 @@
 #samsoft to fasta of the reads that do not map on the alignemnt
-#
-#
+#This script get the reads that map to the database 
+#USE: perl samsoft2fasta_sihits.pl file.sam > hits.fasta 
 
 open (SAMSOFT, $ARGV[0]);
 my @line;
